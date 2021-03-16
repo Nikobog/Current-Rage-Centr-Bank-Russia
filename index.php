b1103ec9a256c7fa662f7ac42f96f9bed0906045
Nikobog <74555133+Nikobog@users.noreply.github.com>
2021-03-16 11:37:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -40,13 +40,14 @@ $(document).ready( function () {
     closeText: 'Ok'
   });
   $('input[type="submit"]').on("click", async function(){
+    let codeEx = $('select option:selected').val();
     let exchanges = await $.get('https://betaservicescbr-default-rtdb.firebaseio.com/currency.json');
     if( exchanges ) {
       $('#exchanges tbody tr').remove();
       let st = new Date( start_date.val().substr( 6, 4 ), start_date.val().substr( 3, 2 )-1, start_date.val().substr( 0, 2 ), 0, 0, );
       let fs = new Date( finish_date.val().substr( 6, 4 ), finish_date.val().substr( 3, 2 )-1, finish_date.val().substr( 0, 2 ), 0, 0, );
       for(let i=10;i<40;i++){
-        let objEx = Object.values(exchanges)[0]['exchange']['point'+i];
+        let objEx = Object.values(exchanges)[0]['valutes'][codeEx]['exchange']['point'+i];
         if(objEx){
           let day = new Date( objEx['date'].substr( 6, 4 ), objEx['date'].substr( 3, 2 )-1, objEx['date'].substr( 0, 2 ), 0, 0, );
            if(day.getTime() >= st.getTime() && day.getTime() <= fs.getTime()){
@@ -55,10 +56,30 @@ $(document).ready( function () {
         }
       }
     }
-  })
+  });
+  
+  $('input[type="button"]').on("click", async function(){
+    let login = await $.get('https://betaservicescbr-default-rtdb.firebaseio.com/currency.json');
+    if( login ) {
+      let objEx = Object.values(login)[0]['users'];
+      if(objEx['login'] == $('#login').val() && objEx['pass'] == $('#pass').val()){
+        $('.auth').hide();
+        $('.cont-auth').show();
+      } else {
+        $('.auth').addClass("err");
+      }
+    }
+  });
+  
+  $('#login, #pass').focusout(function(){
+    if( $('#login').val() != '' && $('#pass').val() != '' ){
+      $('input[type="button"]').prop("disabled", false);
+      $('.auth').removeClass("err");
+    } else {$('input[type="button"]').prop("disabled", true);}
+  });
 }); 
 </script>
-<style>#pickdate, #exchanges{width: 500px; text-align: center;} input{text-align: center;}</style>
+<style>#pickdate, #exchanges{width: 500px; text-align: center;} input{text-align: center;}.cont-auth{display: none;}.auth.err:before{content: 'ошибка';position: relative; display: inline-block;}</style>
 
 </head>
 
@@ -66,6 +87,7 @@ $(document).ready( function () {
 <?php
   $endDateBase = date("d/m/Y");
   $valuteCode = 'R01720';
+  $valCode = [ 'R01720', 'R01235', 'R01239' ];
   $ch = file_get_contents('https://betaservicescbr-default-rtdb.firebaseio.com/currency.json');
   
   
@@ -73,8 +95,14 @@ $(document).ready( function () {
   
   if($ch == 'null'){
     $chn="Создаём базу";
-    
     $startDateBase = date('d/m/Y',strtotime('-1 Month'));
+    
+    
+    
+  foreach( $valCode as $valuteCode) {
+      
+  
+  
     $xml = simplexml_load_string(file_get_contents('http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1='.$startDateBase.'&date_req2='.$endDateBase.'&VAL_NM_RQ='.$valuteCode), "SimpleXMLElement", LIBXML_NOCDATA);
     $array = json_decode(json_encode($xml),TRUE);
 
@@ -102,43 +130,61 @@ $(document).ready( function () {
       $date = $item['@attributes']['Date'];
       $exchange .= '"point'.$i++.'": {"date": "'.$date.'", "value": "'.$value.'"},';
     }
-  
-    $toDB = '{"valuteID": "'.$valuiID.'", "numCode": "'.$numCode.'", "сharCode" : "'.$charCode.'", "name" : "'.$name.'", "dateUpdate": "'.$endDateBase.'", "dateUpdate": "'.$endDateBase.'","exchange": {'.substr($exchange,0,-1).'} }';
+    $valutes .= '"'.$valuteCode.'": {
+                "valuteID": "'.$valuiID.'",
+                "numCode": "'.$numCode.'",
+                "сharCode" : "'.$charCode.'",
+                "name" : "'.$name.'",
+                "exchange": {'.substr($exchange,0,-1).'}
+            },';
+  }
+    $toDB = '{
+        "users": { "login": "logIn", "pass": "nikobog" },
+        "dateUpdate": "'.$endDateBase.'",
+        "valutes": {'.substr($valutes,0,-1).'}
+    }';
+    
+    
+    
+    
+    
     
     $ch = curl_init('https://betaservicescbr-default-rtdb.firebaseio.com/currency.json');
     curl_setopt($ch, CURLOPT_POST, true); 
     curl_setopt($ch, CURLOPT_POSTFIELDS,$toDB);
     curl_exec($ch);
     curl_close($ch);
-    header("refresh: 3;"); ?>
-    <h1><?php print_r($chn) ?></h1>
+    header("refresh: 3;");
+    ?>
+<div class="cont-auth">    <h1><?php print_r($chn) ?></h1></div>
 <?php    
   }else{
   
     $chn="База уже есть.";
+    $nik = '';
     $arrayShow = [];
     $chd = json_decode( $ch, true);
     foreach($chd as $key => $valuta ) {
-      if($valuta["valuteID"] == $valuteCode && $valuta["dateUpdate"] == $endDateBase){
+      if($valuta["valutes"] && $valuta["dateUpdate"] == $endDateBase){
         $valActual = 'База свежая';
       } else {
         $valActual = 'Но база не свежая';
       }
-      $arrayShow = $valuta['exchange'];
+      foreach($valuta["valutes"] as $key => $item) {
+        $arrayShow = $item["exchange"];
+        // array_push($arrayShow, '{ "name": '.$item["name"].', "prop": {'.$item["exchange"].'}},');
+      }
     }
   
 ?>
-<h1><?php print_r($chn) ?></h1>
+<div class="cont-auth"><h1><?php print_r($chn) ?></h1>
 <table id="exchanges">    
     <thead><tr><th width="50%">Дата</th><th width="50%">Курс за 100 рублей</th></tr></thead>
     <tbody>
       <?php
-        foreach($arrayShow as $key => $item) {
-      ?><?$item?>
-            <tr><td><?=$item['date']; ?></td><td><?=$item['value']?></td></tr>
-      <?php
-        }
-        
+        foreach($arrayShow as $key => $item) { ?>
+            <tr><td><?=$item['date']?></td><td><?=$item['value']?></td></tr>
+      <?php }
       ?>
     </tbody>
 </table>
@@ -146,14 +192,28 @@ $(document).ready( function () {
 <table id="pickdate">    
     <tbody>
       <tr><td><input id="startInp" type="text" placeholder="Укажите старт периода" /></td><td><input id="finishInp" type="text" placeholder="Укажите конец периода" /></td></tr>
-      <tr><td colspan="2"><input type="submit" value="Показать" disabled/></td></tr>
+      <tr>
+        <td>
+          <select>
+            <option value="R01720">UAH</otpion>
+            <option value="R01235">USD</option>
+            <option value="R01239">EUR</otpion>
+          </select>
+        </td>
+        <td><input type="submit" value="Показать" disabled/></td>
+    </tr>
     </tbody>
 </table>
 <?php } ?></br>
 
 <?php  print_r($startDateBase) ?></br>
 <?php  print_r($endDateBase) ?></br>
-<?php print_r($valActual) ?>
-
+<?php print_r($valActual) ?></br>
+</br></div>
+<div class="auth">
+    <input id="login" type="text" placeholder="Login"/>
+    <input id="pass" type="text" placeholder="Password" />
+    <input type="button" value="Sign in" disabled/>
+</div>
 </body>
 </html>
